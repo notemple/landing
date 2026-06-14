9338b806b7f5f3cf450e153d6c84ee98df73dc24
n1la9jan <nilanjanmridha89@gmail.com>
2026-06-14 17:09:16 +0530

    fixed errors

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'motion/react';
 import {
-  HardDrive,
   DiscordLogo,
   TwitterLogo,
   RedditLogo,
